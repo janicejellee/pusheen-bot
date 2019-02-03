410129167683a6e65303958539d874db00bf3e4e
Janice Lee <janicelee1120@gmail.com>
2019-02-03 01:43:13 -0800

    Add some holidayz n stuff

--- helpers/processMessageTemplate.js
+++ helpers/processMessageTemplate.js
@@ -52,15 +52,78 @@ module.exports = (event) => {
   apiaiSession.on('response', (response) => {
     const result = response.result.fulfillment.speech;
 
+
+    // basic string checking
+    // - specific holidays
+
+    if (msg.includes('new') && msg.includes('year')) {
+      const current_pusheens = pusheen_imgs['new years'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
+    } else if (msg.includes('valentine')) {
+      const current_pusheens = pusheen_imgs['valentines'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
+    } else if (msg.includes('st') && msg.includes('patrick')) {
+      const current_pusheens = pusheen_imgs['valentines'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
+    } else if (msg.includes('easter')) {
+      const current_pusheens = pusheen_imgs['easter'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
+    } else if (msg.includes('mother')) {
+      const current_pusheens = pusheen_imgs['mothers'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
+    } else if (msg.includes('father')) {
+      const current_pusheens = pusheen_imgs['fathers'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
+    } else if (msg.includes('thanksgiving')) {
+      const current_pusheens = pusheen_imgs['thanksgiving'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
+    } else if (msg.includes('christmas')) {
+      const current_pusheens = pusheen_imgs['christmas'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
     // dialog flow special returns
     // - ACTIVITY-PUSHEEN
     // - PLACE-PUSHEEN
     // - COSTUME-PUSHEEN
     // - BIRTHDAY-PUSHEEN
     // - HOLIDAY-PUSHEEN
+    // - FOOD-PUSHEEN
     // - RANDOM-PUSHEEN
 
-    if (result == 'ACTIVITY-PUSHEEN') {
+    } else if (result == 'HELP-TEXT') {
+      const help_text = "Nya! :3 I'm Pusheen, and I send images and GIFs depending on what you say! \
+Ask me questions! Here are the key phrases and words I can understand. :3 \n\n \
+Question related: \n - what should I do today / what did you do today / etc. \n \
+- where should I go / where did you go / etc. \n - costume / what do you want to be for Halloween \n\n \
+Key phrases: \n - birthday \n - holiday \n - specific holiday (e.g. christmas) \n - random (random pusheen!) \n\n \
+And, just type 'help' to get this message again! :3";
+
+      sendTextMessage(senderId, help_text);
+
+    } else if (result == 'ACTIVITY-PUSHEEN') {
       const current_pusheens = pusheen_imgs['activities'];
       const i = Math.floor(Math.random() * current_pusheens.length);
       const random_pusheen = current_pusheens[i];
@@ -74,6 +137,13 @@ module.exports = (event) => {
 
     } else if (result == 'COSTUME-PUSHEEN') {
       const current_pusheens = pusheen_imgs['costumes'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
+    // for the specific holiday
+    } else if (msg.includes('halloween')) {
+      const current_pusheens = pusheen_imgs['halloween'];
       const i = Math.floor(Math.random() * current_pusheens.length);
       const random_pusheen = current_pusheens[i];
       sendImage(senderId, random_pusheen);
@@ -97,6 +167,12 @@ module.exports = (event) => {
       sendTextMessage(senderId, 'Nya! I love you too! :3');
       sendImage(senderId, random_pusheen);
 
+    } else if (result == 'FOOD-PUSHEEN') {
+      const current_pusheens = pusheen_imgs['foods'];
+      const i = Math.floor(Math.random() * current_pusheens.length);
+      const random_pusheen = current_pusheens[i];
+      sendImage(senderId, random_pusheen);
+
     } else if (result == 'RANDOM-PUSHEEN') {
       // select random category
       const i = Math.floor(Math.random() * CATEGORIES.length);
@@ -106,9 +182,6 @@ module.exports = (event) => {
       const j = Math.floor(Math.random() * current_pusheens.length);
       const random_pusheen = current_pusheens[i];
       sendImage(senderId, random_pusheen);
-
-    // TODO basic string checking
-    // - specific holidays
 
     } else {
       sendTextMessage(senderId, result);
